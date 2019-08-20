2b4e9d5b34056b688c09d6ad56ff0d2e56dc7f7b
Alexander Campbell <acampbell2231@gmail.com>
2019-08-20 01:57:00 -0700

    All core functionality built out

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -2,16 +2,20 @@ import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Repos from '../repos/Repos';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
   };
 
   render() {
@@ -31,7 +35,7 @@ export class User extends Component {
       hireable
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -96,14 +100,11 @@ export class User extends Component {
         </div>
         <div className='card text-center'>
           <div className='badge badge-primary'>Followers: {followers}</div>
-          <div className='badge badge-primary'>Following: {following}</div>
-          <div className='badge badge-primary'>
-            Public Repos: {public_repos}
-          </div>
-          <div className='badge badge-primary'>
-            Public Gists: {public_gists}
-          </div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </Fragment>
     );
   }
